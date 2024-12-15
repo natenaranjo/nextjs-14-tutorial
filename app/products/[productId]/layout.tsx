a1aa47121b46d41fd718e5c9752ef6b9d844e049
Nate Naranjo <natenaranjodev@gmail.com>
2024-12-15 14:58:10 -0600

    feat(layout): added nested layout in product

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function ProductDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {children}
+      <h2>Feature products</h2>
+    </>
+  )
+}
