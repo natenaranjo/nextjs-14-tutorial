49e649f7e2aec1e6cb2c8e92491a3f8122d6dea8
Nate Naranjo <natenaranjodev@gmail.com>
2024-12-19 14:46:37 -0600

    feat(metadata): create static and dynamic metadata

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
-type ParamsType = Promise<{ productId: string }>
+import { Metadata } from "next"
 
-export default async function ProductPage({ params }: { params: ParamsType }) {
-  const { productId } = await params
-  return <h1>Details on product {productId}</h1>
+type Props = {
+  params: Promise<{
+    productId: string
+  }>
+}
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  return {
+    title: `Product ${(await params).productId}`,
+  }
+}
+
+export default async function ProductPage({ params }: Props) {
+  return <h1>Details on product {(await params).productId}</h1>
 }
